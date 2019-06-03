51db8359693c698da9f427e4e30d1a574949fdbf
surjsi <surjit.konjengbam@gmail.com>
2019-06-02 23:23:14 -0400

    Update build-netlify.sh

--- build-netlify.sh
+++ build-netlify.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
+# This script prepares the environment for Netlify builds.
+# - Installs comply
+# - Installs pdflatex
+# - Installs extra LaTeX packages if necessary
+# - Puts comply and pdflatex exacutables into the PATH
+# - Generates a comply.yml file from env variables
+# - Runs the main build script
+
+# PARAMETERS
+# This folder remains available across builds. Let's use it for extra dependencies
 if [ -w "/opt/build/cache" ]; then
   CACHE_FOLDER="/opt/build/cache"
 else
   CACHE_FOLDER="$HOME/cache"
 fi
-
-## Installing comply ##
-
-go get -v github.com/strongdm/comply
-
-## Installing texlive ##
-
-# space-delimited list of extra latex packages to install
-# See https://www.ctan.org/pkg/[latexpackage] to find the texlive bundle
-LATEXPACKAGES="mdwtools graphics oberdiek setspace amsmath eurosym upquote fancyvrb polyglossia natbib listings tools booktabs parskip bidi fancyhdr"
-
-# Netlify Xenial images don't have a texlive install, so we need to install it on first build.
-# We're putting it in the `/opt/build/cache` folder, which is kept between builds
-# The following folder is mentioned in netlify-texlive.profile
+# EXTRA_TEXLIVE_PACKAGES: array of TeX Live packages to install
+# See https://www.ctan.org/pkg/[latexpackage] to find the TeX Live package name
+# The following are already present by default: mdwtools graphics oberdiek setspace amsmath eurosym upquote fancyvrb polyglossia natbib listings tools booktabs parskip fancyhdr
 TEXLIVEDIR="$CACHE_FOLDER/texlive"
-TEXLIVEBINDIR="$TEXLIVEDIR/bin/x86_64-linux"
 
-export PATH="$PATH:$TEXLIVEBINDIR:$GOPATH/bin"
+export PATH="$PATH:$TEXLIVEDIR/bin/x86_64-linux:$GOPATH/bin"
 
+# Install comply
+UPSTREAM="github.com/strongdm/comply"
+FORK="github.com/ridem/comply"
+
+echo "Installing $UPSTREAM"
+go get $UPSTREAM
+echo "Rebuilding with changes from $FORK"
 # Installing "dep" to "$GOPATH/bin" if absent
 if ! type dep > /dev/null 2>&1 ; then
   curl -s https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 fi
+(
+cd $GOPATH/src/$UPSTREAM
+git remote | grep fork > /dev/null || git remote add fork "https://$FORK.git"
+git fetch -q fork master && git reset -q --hard fork/master
+dep ensure
+go get ./...
+)
 
-# Let's test that a pdflatex executable is present:
 if ! type pdflatex > /dev/null 2>&1 ; then
   echo "Installing TeX Live (pdflatex)"
   wget -q http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
@@ -52,14 +63,13 @@ else
   echo "TeX Live already installed, proceeding."
 fi
 
-# Installing dependencies. If they're already present, they won't be rebuilt.
-for val in $LATEXPACKAGES; do
-    tlmgr install "$val" || echo "Couldn't install the following LaTeX package: $val"
-done
+# Installing dependencies. If they're already present in Netlify Cache, they won't be reinstalled.
+# tlmgr install bidi sourcesanspro sourceserifpro
 
 # Building "comply.yml" by substituting ENV variable set on netlify (e.g. GITHUB_TOKEN)
 envsubst < comply.dist.yml > comply.yml
 
-# All dependencies are installed (from the second build, everything will be loaded from the cache)
-# Let's build the documents now!
+echo "Building the comply site"
+# Running the core build script, which you can run locally
+#./build-site.sh
 ./run-comply.sh
